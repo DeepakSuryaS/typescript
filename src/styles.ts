01023b37c07a6a7c1ac069da1454cb8c91d42c32
Deepak Surya <ideepaksuryas@gmail.com>
2021-09-21 23:00:30 +0530

    add column container

--- src/styles.ts
+++ src/styles.ts
@@ -9,3 +9,13 @@ export const AppContainer = styled.div`
   padding: 20px;
   background: #3179ba;
 `;
+
+export const ColumnContainer = styled.div`
+  flex-grow: 0;
+  width: 300px;
+  min-height: 40px;
+  padding: 8px;
+  margin-right: 20px;
+  border-radius: 3px;
+  background: #ebecf0;
+`;
