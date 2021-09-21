59d750fe316cebb9fe1df6ddf85fec4f35d98d81
Deepak Surya <ideepaksuryas@gmail.com>
2021-09-21 22:57:29 +0530

    add app container

--- src/styles.ts
+++ src/styles.ts
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  align-items: flex-start;
+  padding: 20px;
+  background: #3179ba;
+`;
