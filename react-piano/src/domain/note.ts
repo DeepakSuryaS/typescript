0e7d156db8699daac14307108a0ea2017f20b425
Deepak Surya <ideepaksuryas@gmail.com>
2021-10-27 23:20:17 +0530

    setup Note entity type

--- react-piano/src/domain/note.ts
+++ react-piano/src/domain/note.ts
@@ -4,3 +4,11 @@ export type OctaveIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 export type MidiValue = number;
 export type PitchIndex = number;
+
+export type Note = {
+  midi: MidiValue;
+  type: NoteType;
+  pitch: NotePitch;
+  index: PitchIndex;
+  octave: OctaveIndex;
+};
