3e30a1c709f18d8f25a21741a1de08081bf324a2
Deepak Surya <ideepaksuryas@gmail.com>
2021-11-16 22:08:05 +0530

    add press observer

--- react-piano/src/components/Key/Key.tsx
+++ react-piano/src/components/Key/Key.tsx
@@ -1,20 +1,29 @@
-import { FunctionComponent, ReactEventHandler } from "react";
+import { FunctionComponent } from "react";
+import clsx from "clsx";
 import { NoteType } from "../../domain/note";
+import { usePressObserver } from "../PressObserver/usePressObserver";
+import styles from "./Key.module.css";
 
+type PressCallback = () => void;
 type KeyProps = {
   type: NoteType;
   label: string;
   disabled?: boolean;
-  onUp: ReactEventHandler<HTMLButtonElement>;
-  onDown: ReactEventHandler<HTMLButtonElement>;
+  onUp: PressCallback;
+  onDown: PressCallback;
 };
 
 const Key: FunctionComponent<KeyProps> = (props) => {
   const { type, label, onDown, onUp, ...rest } = props;
+  const pressed = usePressObserver({
+    watchKey: label,
+    onStartPress: onDown,
+    onFinishPress: onUp,
+  });
 
   return (
     <button
-      className={clsx(styles.key, styles[type])}
+      className={clsx(styles.key, styles[type], pressed && "is-pressed")}
       type="button"
       onMouseDown={onDown}
       onMouseUp={onUp}
