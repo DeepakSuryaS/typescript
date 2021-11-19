bcd12799456f344f92b05feaf2ea08623ac38450
Deepak Surya <ideepaksuryas@gmail.com>
2021-11-19 21:13:56 +0530

    refactor with render props

--- react-piano/src/components/Keyboard/WithInstrument.tsx
+++ react-piano/src/components/Keyboard/WithInstrument.tsx
@@ -3,17 +3,26 @@ import { useSoundfont } from "../adapters/Soundfont/useSoundfont";
 import Keyboard from "./Keyboard";
 import { useInstrument } from "../../state/Instrument/Context";
 import { useEffect } from "react";
+import { SoundfontProvider } from "../../adapters/Soundfont/SoundfontProvider";
 
 const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
   const { instrument } = useInstrument();
-  const { loading, current, play, stop, load } = useSoundfont({ AudioContext });
+  // const { loading, current, play, stop, load } = useSoundfont({ AudioContext });
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (!loading && instrument !== current) load(instrument);
   }, [load, loading, current, instrument]);
 
-  return <Keyboard loading={loading} play={play} stop={stop} />;
+  return <Keyboard loading={loading} play={play} stop={stop} />; */
+
+  return (
+    <SoundfontProvider
+      AudioContext={AudioContext}
+      instrument={instrument}
+      render={(props) => <Keyboard {...props} />}
+    />
+  );
 };
 
 export default KeyboardWithInstrument;
