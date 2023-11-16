f9652054338e4bb9c67e1fad4f71e74627af6ecf
Deepak Surya <ideepaksuryas@gmail.com>
2023-11-16 22:07:12 +0530

    add: define CourseGoal prop type

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -3,7 +3,7 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal title="Learn React + TS" description="From the ground up" />
     </main>
   );
 }
