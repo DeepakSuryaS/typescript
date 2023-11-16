a36d6b4c64fdfc309683c9c723dc1bdc7d1b8b4b
Deepak Surya <ideepaksuryas@gmail.com>
2023-11-16 22:19:36 +0530

    improve: refactor with children prop

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -3,7 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal title="Learn React + TS" description="From the ground up" />
+      <CourseGoal title="Learn React + TS">
+        <p>From the ground up</p>
+      </CourseGoal>
     </main>
   );
 }
--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,14 +1,23 @@
-type CourseGoalProps = {
-  title: string;
-  description: string;
-};
+import { type PropsWithChildren, type ReactNode } from "react";
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+// the "type" keyword before the imported items serves as a decorator for the bundlers
+// it says that the imported items are just type definitions and that the browser won't be able to do anything with it
+// so it can be safely ignored while bundling
+
+interface CourseGoalProps {
+  title: string;
+  children: ReactNode;
+}
+
+// below is another way
+// type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>
