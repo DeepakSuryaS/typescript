93ba3a9782baa58c595c20aa16fbc4fb60dc47b7
Deepak Surya <ideepaksuryas@gmail.com>
2023-11-16 22:25:41 +0530

    add: functional comp props

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,4 +1,4 @@
-import { type PropsWithChildren, type ReactNode } from "react";
+import { type FC, type PropsWithChildren, type ReactNode } from "react";
 
 // the "type" keyword before the imported items serves as a decorator for the bundlers
 // it says that the imported items are just type definitions and that the browser won't be able to do anything with it
@@ -23,3 +23,17 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
     </article>
   );
 }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
