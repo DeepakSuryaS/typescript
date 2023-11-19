fd87aa9efdb580d03db425c7790c85982f9576c4
Deepak Surya <ideepaksuryas@gmail.com>
2023-11-19 22:38:25 +0530

    add: infobox for no and too many goals

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,6 +1,7 @@
-import { type FC } from "react";
+import { ReactNode, type FC } from "react";
 import CourseGoal from "./CourseGoal";
 import { CourseGoalType } from "../App";
+import InfoBox from "./InfoBox";
 
 interface CourseGoalListProps {
   goals: CourseGoalType[];
@@ -8,16 +9,37 @@ interface CourseGoalListProps {
 }
 
 const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDelete }) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length > 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map(({ title, description, id }) => (
-        <li key={id}>
-          <CourseGoal id={id} title={title} onDelete={onDelete}>
-            <p>{description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map(({ title, description, id }) => (
+          <li key={id}>
+            <CourseGoal id={id} title={title} onDelete={onDelete}>
+              <p>{description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
