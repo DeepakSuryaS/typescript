71d577dfbe269421a11a6467e5bc22aac92284a6
Deepak Surya S <ideepaksuryas@gmail.com>
2024-01-08 19:31:04 +0530

    add: start stop and clear timer functionality

--- react-ts/state-management/src/components/Timer.tsx
+++ react-ts/state-management/src/components/Timer.tsx
@@ -1,11 +1,43 @@
-import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from "react";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
+import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const timerRef = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000); // multiple by 1000 to convert to milliseconds
+
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && timerRef.current) {
+    clearInterval(timerRef.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prev) => (prev <= 0 ? prev : prev - 50));
+      }, 50);
+      timerRef.current = timer;
+    } else if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
