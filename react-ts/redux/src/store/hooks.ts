33ae9f0aa5329f40beab2030f3cfe75dbb6eb709
Deepak Surya <ideepaksuryas@gmail.com>
2024-01-09 13:45:30 +0530

    improve: integrate add to cart func

--- react-ts/redux/src/store/hooks.ts
+++ react-ts/redux/src/store/hooks.ts
@@ -1,6 +1,7 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "./store";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import { AppDispatch, RootState } from "./store";
 
 type DispatchFuncType = () => AppDispatch;
 
 export const useCartDispatch: DispatchFuncType = useDispatch;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
