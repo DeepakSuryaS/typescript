315a0970be846c5cea73e5eeb950017e7655c933
Deepak Surya <ideepaksuryas@gmail.com>
2024-01-09 13:10:51 +0530

    add: reducer logic for add and remove

--- react-ts/redux/src/store/cart-slice.ts
+++ react-ts/redux/src/store/cart-slice.ts
@@ -1,5 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+};
+
+type CartStateType = {
+  items: CartItemType[];
+};
+
+type AddToCartActionType = PayloadAction<{
+  id: number;
+  title: string;
+  price: number;
+}>;
+
+type RemoveFromCartActionType = PayloadAction<string>;
+
+const initialState: CartStateType = {
+  items: [],
+};
 
 export const cartSlice = createSlice({
   name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: AddToCartActionType) => {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+    removeFromCart: (state, action: RemoveFromCartActionType) => {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === parseInt(action.payload)
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity -= 1;
+      }
+    },
+  },
 });
